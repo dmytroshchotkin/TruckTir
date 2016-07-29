c72e21e53f13c1753669c78a62ae38346692936a
Razor23Donetsk <razorsvetik@mail.ru>
2016-07-29 19:59:35 +0300

    Добавил static class Markup.

---
+++ PartsApp/Models/Markup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PartsApp.SupportClasses;
+
+namespace PartsApp.Models
+{
+    public static class Markup
+    {
+        public enum Types
+        {
+            [System.ComponentModel.Description("Мелкий опт")]
+            SmallWholesale = 75,
+
+            [System.ComponentModel.Description("Средний опт")]
+            AverageWholesale = 50,
+
+            [System.ComponentModel.Description("Крупный опт")]
+            LargeWholesale = 30,
+
+            [System.ComponentModel.Description("Розница")]
+            Retail = 100
+        }//Types
+
+        public static string GetDescriptions(int markup)
+        {
+            if (markup <= 0)
+                return "Уценка";
+
+            switch (markup)
+            {
+                case (int)Types.Retail:
+                    return Types.Retail.ToDescription();
+                case (int)Types.SmallWholesale:
+                    return Types.SmallWholesale.ToDescription();
+                case (int)Types.AverageWholesale:
+                    return Types.AverageWholesale.ToDescription();
+                case (int)Types.LargeWholesale:
+                    return Types.LargeWholesale.ToDescription();
+                default:
+                    return "Другая наценка";
+            }//switch
+        }//GetDescriptions
+
+        public static Dictionary<int, string> GetValues()
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            foreach (Markup.Types item in Enum.GetValues(typeof(Markup.Types)))
+            {
+                dict.Add((int)item, item.ToDescription());
+            }//foreach
+
+            return dict;
+        }//GetValues
+    }//Markup
+
+
+}//namespace
--- PartsApp/SupportClasses/ExtensionClasses.cs
+++ PartsApp/SupportClasses/ExtensionClasses.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace PartsApp.SupportClasses
 {
@@ -20,6 +21,16 @@ namespace PartsApp.SupportClasses
                 .Aggregate((max, next) => next.Item2.CompareTo(max.Item2) < 0 ? next : max).Item1;
         }//MinBy
 
+        /// <summary>
+        /// Возвращает заданное описание для данного перечислителя.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToDescription(this Enum value)
+        {
+            var da = (DescriptionAttribute[])(value.GetType().GetField(value.ToString())).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return da.Length > 0 ? da[0].Description : value.ToString();
+        }
     }//EnumerableExtensions
 
 
