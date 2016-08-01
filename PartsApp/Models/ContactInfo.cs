dcd9787f0017f730edff2f5d3c4d82ae3d8cbed4
Razor23Donetsk <razorsvetik@mail.ru>
2016-08-01 23:20:01 +0300

    Добавил новый файл Models\ContactInfo.cs и перенес туда класс ContactInfo. Удалил файл ContactInfo.cs .

---
+++ PartsApp/Models/ContactInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsApp.Models
+{
+    public class ContactInfo
+    {
+        public int ContactInfoId { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Room { get; set; }
+        public string Phone { get; set; }
+        public string ExtPhone { get; set; }
+        public string Website { get; set; }
+        public string Email { get; set; }
+
+
+        public ContactInfo() { }
+        public ContactInfo(string country, string region, string city, string street, string house,
+                           string room, string phone, string extphone, string website, string email)
+        {
+            Country = country;
+            Region = region;
+            City = city;
+            Street = street;
+            House = house;
+            Room = room;
+            Phone = phone;
+            ExtPhone = extphone;
+            Website = website;
+            Email = email;
+        }//
+        public ContactInfo(int contactinfoid, string country, string region, string city, string street, string house,
+                           string room, string phone, string extphone, string website, string email)
+        {
+            ContactInfoId = contactinfoid;
+            Country = country;
+            Region = region;
+            City = city;
+            Street = street;
+            House = house;
+            Room = room;
+            Phone = phone;
+            ExtPhone = extphone;
+            Website = website;
+            Email = email;
+        }//
+
+
+
+    }//ContactInfo
+}
