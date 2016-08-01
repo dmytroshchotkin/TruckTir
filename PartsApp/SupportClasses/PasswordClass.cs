784a7aa7e6757bf0b0d61e708ca509abd48c556a
Razor23Donetsk <razorsvetik@mail.ru>
2016-08-01 23:29:58 +0300

    Добавил новый файл SupportClasses\PasswordClass.cs и перенес туда класс PasswordClass. Удалил файл PasswordClass.cs .

--- PartsApp/SupportClasses/PasswordClass.cs
+++ PartsApp/SupportClasses/PasswordClass.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
-namespace PartsApp
+namespace PartsApp.SupportClasses
 {
     public class PasswordClass
     {
