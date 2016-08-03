4c309417fcff72a22c23068f83671582c67736d8
Razor23Donetsk <razorsvetik@mail.ru>
2016-08-03 12:00:33 +0300

    IOperation. Убрал атрибуты DisplayName со свойств.

--- PartsApp/Models/IContragent.cs
+++ PartsApp/Models/IContragent.cs
@@ -8,25 +8,13 @@ namespace PartsApp.Models
 {
     public interface IContragent
     {
-        //[Browsable(false)]
         int ContragentId { get; set; }
-
-        [System.ComponentModel.DisplayName("Название")]
         string ContragentName { get; set; }
-
-        [System.ComponentModel.DisplayName("ИНН/ОКПО")]
         string Code { get; set; }
-
-        [System.ComponentModel.DisplayName("Юр./Физ. лицо")]
         string Entity { get; set; }
-
-        [System.ComponentModel.Browsable(false)]
         ContactInfo ContactInfo { get; set; }
-
-        [System.ComponentModel.DisplayName("Описание")]
         string Description { get; set; }
     }//IContragent
-
 
 }//namespace
 
