d67f9cb523ef1cfb606b2a520a0aa51b01bae7c8
Razor23Donetsk <razorsvetik@mail.ru>
2016-08-16 14:01:38 +0300

    EnumerableExtensions. Добавил методы GetAllControls(this Control control, Type type) и GetAllControls(this Control control, Type type, string searchName).

--- PartsApp/SupportClasses/ExtensionClasses.cs
+++ PartsApp/SupportClasses/ExtensionClasses.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace PartsApp.SupportClasses
 {
@@ -32,6 +33,37 @@ namespace PartsApp.SupportClasses
             return da.Length > 0 ? da[0].Description : value.ToString();
         }//ToDescription
 
+
+        /// <summary>
+        /// Возвращает список всех контролов заданного типа, расположенных на форме.
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="type">Тип контролов которые требуется найти.</param>
+        /// <returns></returns>
+        public static List<Control> GetAllControls(this Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAllControls(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type).ToList();
+        }//GetAllControls
+
+        /// <summary>
+        /// Возвращает список всех контролов заданного типа и имеющие совпадение в имени, расположенных на форме.
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="type">Тип контролов которые требуется найти.</param>
+        /// <param name="searchName">Полное или частичное имя контрола.</param>
+        /// <returns></returns>
+        public static List<Control> GetAllControls(this Control control, Type type, string searchName)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAllControls(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type && c.Name.Contains(searchName)).ToList();
+        }//GetAllControls
     }//EnumerableExtensions
 
 
