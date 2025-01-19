0f692a7f2819b9815df9d391f41d69765cf324b0
Anna <anka.dubr@gmail.com>
2025-01-19 23:03:00 +0300

    #48 Set purchase price in Returns by mean price from last Purchases

---
+++ BusinessLogic/ReturnPriceHandler.cs
@@ -0,0 +1,23 @@
+﻿using PartsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public class ReturnPriceHandler
+    {
+        public OperationDetails OperationDetails { get; }
+
+        public ReturnPriceHandler(OperationDetails od)
+        {
+            OperationDetails = od;
+        }
+
+        public void SetMeanPrice(List<OperationDetails> operations)
+        {
+            float meanPrice = (float)Math.Round(operations.Average(od => od.Price));
+            OperationDetails.Price = meanPrice;
+        }
+    }
+}
